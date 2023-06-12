b2ab80dbe5ac680625c72b02fd08b38cedf169fc
Jason Ren <40999116+jasonren0403@users.noreply.github.com>
2023-06-12 11:27:20 +0000

    use colored logs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import random
 import logging
 
 import requests
+import coloredlogs
 from requests.adapters import HTTPAdapter
 
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35"
@@ -17,6 +18,7 @@ url_dict = {
 
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger("rank-data-crawler")
+coloredlogs.install(level="INFO", logger=logger)
 
 def get_proxy():
     # https://github.com/TheSpeedX/PROXY-List
