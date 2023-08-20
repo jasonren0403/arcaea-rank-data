b926d33b6b88dd4d41e6e529ef339dcdb74805d4
Jason Ren <40999116+jasonren0403@users.noreply.github.com>
2023-08-20 13:55:21 +0000

    fix & improve logging

--- filecheck.py
+++ filecheck.py
@@ -8,15 +8,18 @@ year = today.year
 month = today.month
 day = today.day
 
+logging.basicConfig(level="INFO",
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger("file-checker")
 
-TEST=False
+TEST=os.getenv('CI', 'false') == 'false'
 
 def filecheck(file_name) -> bool:
     path = pathlib.Path('./') / str(year) / str(month) / str(day) / file_name
     if TEST:
         print(f"path {path.as_posix()} exists:", path.exists())
-    logger.info('%s exists: %s', str(path), path.exists())
+    else:
+        logger.info('%s exists: %s', path.as_posix(), path.exists())
     return path.exists()
 
 if __name__ == "__main__":
